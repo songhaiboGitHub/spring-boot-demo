535d94be6461acb5b17dc239e4d8aaad03027fdc
songhaibo <songhaibo@winzct.com>
2022-12-12 21:03:23 +0800

    feat: 增加 mongoDB

--- demo-mongodb/src/test/java/com/xkcoding/mongodb/repository/ImgeRepositoryTest.java
+++ demo-mongodb/src/test/java/com/xkcoding/mongodb/repository/ImgeRepositoryTest.java
@@ -1,30 +1,24 @@
 package com.xkcoding.mongodb.repository;
 
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Snowflake;
-import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
 import com.google.common.collect.Lists;
 import com.xkcoding.mongodb.SpringBootDemoMongodbApplicationTests;
-import com.xkcoding.mongodb.model.Article;
 import com.xkcoding.mongodb.model.ImagePO;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONObject;
 import org.junit.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -95,6 +89,44 @@ public class ImgeRepositoryTest extends SpringBootDemoMongodbApplicationTests {
         mongoTemplate.insertAll(imagePOS);
         log.info("【articles】= {}", JSONUtil.toJsonStr(imagePOS.stream().map(ImagePO::getId).collect(Collectors.toList())));
     }
+
+
+    /**
+     * 测试分页排序查询
+     */
+    @Test
+    public void testQuery() {
+
+        Criteria criteria = Criteria.where("equipmentNum").in(Arrays.asList("G28206747"));
+            criteria.and("type").is("0");
+
+        String format_DateTime = "yyyy-MM-dd HH:mm:ss";
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(format_DateTime);
+        LocalDateTime localDateTime = LocalDateTime.parse("2023-11-23 12:13:08", df);
+        //创建时间  开始时间  结束时间
+            criteria = criteria.and("time").lte(localDateTime);
+//        if (dto.getTimeBeg() != null && dto.getTimeEnd() != null) {
+//            criteria.andOperator(
+//                Criteria.where("time").gte("2021-01-01 12:12:12"),
+//                Criteria.where("time").lte("2022-12-29 12:12:12"));
+//        } else {
+//            if (dto.getTimeBeg() != null) {
+//        criteria.and("time").gte("2021-01-01 12:12:12.000").lte("2023-12-29 12:12:12.000");
+//        criteria= criteria.and("time").gte("2021-01-01 12:12:12");
+//            }
+//            if (dto.getTimeEnd() != null) {
+//                criteria.and("time").lte(DateUtils.formatDateTime(dto.getTimeEnd()));
+//            }
+//        }
+        Query query = Query.query(criteria).with(Sort.by(Sort.Direction.DESC, "time"));
+        long total = mongoTemplate.count(query, ImagePO.class);
+        query.skip((1 - 1) * 10).limit(10);
+        List<ImagePO> list = mongoTemplate.find(
+            query, ImagePO.class
+        );
+        log.info("【总页数】= {}", list);
+    }
+
 
 
 }
